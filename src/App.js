f6bd8627dc43a1fec9a6c8b042decb8bcd79deac
cade.faulkner <cadefaulkner@gmail.com>
2022-10-14 18:28:44 +0100

    funny

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
+const funnyVine = new Audio("https://cdn.discordapp.com/attachments/960962795447386112/1030514988399480933/vine-boom.mp3")
+
 function App() {
+  function teeHeHe(){
+     funnyVine.play();
+  }
+
   return (
-    <div className="App">
+    <div className="App" >
       <p class="head">Olof.Rollsby.Soy!</p>
       <div class="fan-message">
         <p>Olof Rollsby number one supper fan site!!!</p>
@@ -15,7 +21,7 @@ function App() {
       </p>
       <div class="grid-container">
         <div class="main">
-          <img src="pictures/olof1.png" alt="Girl in a jacket" class="kewlImage"/>
+          <img src="pictures/olof1.png" alt="Girl in a jacket" class="kewlImage" onClick={() => teeHeHe()} />
           <img src="pictures/olofsoy.jpg" alt="Girl in a jacket soy" class="kewlImage"/>
         </div>
         <div class="flop-left">
@@ -28,5 +34,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
